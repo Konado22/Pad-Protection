4039f58dc2aed3670fbfbc6bcca547b7f0544c1f
unknown <jessedraper2@gmail.com>
2022-02-15 13:35:24 -0700

    cleanup merge

--- client/src/App.js
+++ client/src/App.js
@@ -9,42 +9,10 @@ import { setContext } from "@apollo/client/link/context";
 import React from "react";
 import MainContainer from './pages/mainContainer'
 
-<<<<<<< HEAD
-import Homepage from "./pages/homepage/homepage";
-// import Detail from "./pages/Detail";
-// import NoMatch from "./pages/NoMatch";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-// import Nav from "./components/navbar/navbar";
-import { StoreProvider } from "./utils/GlobalState";
-// import Success from "./pages/Success";
-// import OrderHistory from "./pages/OrderHistory";
-
-const httpLink = createHttpLink({
-  uri: "/graphql",
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
-=======
->>>>>>> ReactJesse
-
 function App() {
   return (
     <ApolloProvider client={client}>
-<<<<<<< HEAD
+
       <Router>
         <div>
           <StoreProvider>
@@ -61,9 +29,7 @@ function App() {
           </StoreProvider>
         </div>
       </Router>
-=======
-      <MainContainer />
->>>>>>> ReactJesse
+
     </ApolloProvider>
   );
 }
