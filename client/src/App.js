812e0ca5daa9c79af0235243b6809fd1b5648bce
msp4msps <msp4msps@tminus365.com>
2022-02-16 20:49:53 -0700

    starter for landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-// need imports 
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
@@ -6,64 +7,42 @@ import {
   createHttpLink,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-import React from "react";
-import MainContainer from './pages/mainContainer'
+import Homepage from "./pages/homepage";
+import Navbar from "./components/navbar/navbar";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <ApolloProvider client={client}>
-
       <Router>
-        <div>
-          <StoreProvider>
-            {/* <Nav /> */}
-            <Switch>
-              {/* <Route exact path="/" component={Homepage} /> */}
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              {/* <Route exact path="/success" component={Success} />
-              <Route exact path="/orderHistory" component={OrderHistory} />
-              <Route exact path="/products/:id" component={Detail} />
-              <Route component={NoMatch} /> */}
-            </Switch>
-          </StoreProvider>
-        </div>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
       </Router>
-
     </ApolloProvider>
   );
 }
 
 export default App;
-
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-// import Home from "./pages/homepage";
-// import Detail from "./pages/Detail";
-// import NoMatch from "./pages/NoMatch";
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
-// import Nav from "./components/Nav";
-// import { StoreProvider } from "./utils/GlobalState";
-// import Success from "./pages/Success";
-// import OrderHistory from "./pages/OrderHistory";
-
-// const httpLink = createHttpLink({
-//   uri: "/graphql",
-// });
-
-// const authLink = setContext((_, { headers }) => {
-//   const token = localStorage.getItem("id_token");
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : "",
-//     },
-//   };
-// });
-
-// const client = new ApolloClient({
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
-
--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,98 +1,23 @@
-import React, { useState } from "react";
-const [currentPage, setCurrentPage] = useState("short");
+import React from "react";
+import "../index.css";
 
-export const Homepage = () => {
-  const user = req.session.user;
-  if (req.session === loggedIn) {
-    return (
-      <div>
-        <div classname="homepageContainer">
-          <h1>Hi {user}</h1>
-
-          <div className="graph card">
-            <h1>Coverage graph here</h1>
-          </div>
-          <div>
-            <div className="pyp">
-              <h1> Protect your Pad</h1>
-              <button className="btn btn-secondary">+ Add Home</button>
-              <img>pic for attention</img>
-            </div>
-            <div className="policies">
-              <input type={checkbox}></input>
-              <p>company</p>
-              <input type={checkbox}></input>
-              <p>company</p>
-              <button>View All</button>
-            </div>
-            <div className="rooms">
-              {setCurrentPage('short')}
-              <Assets />
-            </div>
-            <div className="popularProviders">
-              <h1></h1>
-              <h1></h1>
-            </div>
-          </div>
+const Homepage = () => {
+  return (
+    <div className="api-section api-coverage">
+      <div className="overlay">
+        <div className="clouds">
+          <div className="clouds-bg"></div>
+        </div>
+        <div className="inner">
+          <h2>Protect your Pad</h2>
+          <p>
+            Pad Protection documents your personal property and allows you to
+            trakc all of your policies in one spot!
+          </p>
         </div>
       </div>
-    );
-  } else {
-    //non logged-in render page
-    return(
-      <div>
-        <h1>Pad Protection.this is page for non-logged-in user</h1>
-      </div>
-    )
-  }
+    </div>
+  );
 };
-// import React from "react";
-// import { Link } from "react-router-dom";
 
-// import { useQuery } from "@apollo/client";
-// import { QUERY_USER } from "../utils/queries";
-
-// function Assets() {
-//   const { data } = useQuery(QUERY_USER);
-//   let user;
-
-//   if (data) {
-//     user = data.user;
-//   }
-
-//   return (
-//     <>
-//       <div className="container my-1">
-//         <Link to="/">← Back to Products</Link>
-
-//         {user ? (
-//           <>
-//             <h2>
-//               Order History for {user.firstName} {user.lastName}
-//             </h2>
-//             {user.assets.map((asset) => (
-//               <div key={asset._id} className="my-2">
-//                 <h3>{asset.name}</h3>
-//                 {/* <div className="flex-row">
-//                   {assets.products.map(({ _id, image, name, price }, index) => (
-//                     <div key={index} className="card px-1 py-1">
-//                       <Link to={`/products/${_id}`}>
-//                         <img alt={name} src={`/images/${image}`} />
-//                         <p>{name}</p>
-//                       </Link>
-//                       <div>
-//                         <span>${price}</span>
-//                       </div>
-//                     </div>
-//                   ))}
-//                 </div> */}
-//               </div>
-//             ))}
-//           </>
-//         ) : null}
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Assets;
+export default Homepage;
